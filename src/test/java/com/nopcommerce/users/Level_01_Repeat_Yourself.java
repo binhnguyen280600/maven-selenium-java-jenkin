e6464cd31f2a939147603bcf9bc6a6d8f8c99094
Nguyen Le Nhu Binh <nhubinh280600@gmail.com>
2026-02-19 18:18:43 +0700

    Jenkin cource code update

--- src/test/java/com/nopcommerce/users/Level_01_Repeat_Yourself.java
+++ src/test/java/com/nopcommerce/users/Level_01_Repeat_Yourself.java
@@ -14,62 +14,33 @@ import java.util.Random;
 
 public class Level_01_Repeat_Yourself {
     WebDriver driver;
-    String emailAddress;
-    String firstName;
-    String lastName;
-    String password;
-    String company;
+
 
     @BeforeClass
     public void beforeClass() {
         driver = new FirefoxDriver();
-        driver.get("https://demo.nopcommerce.com/");
+        driver.get("https://www.facebook.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        firstName = "binh";
-        lastName = "nguyen";
-        password = "Abc13579";
-        company = "Goyoung";
-        emailAddress = "nhubinh" + generateRandomNumber ()  + "@gmail.com";
+
     }
 
     @Test
     public void TC_01_Register() {
-        driver.findElement(By.cssSelector("a.ico-register")).click();
-        driver.findElement(By.cssSelector("input#gender-female")).click();
-        driver.findElement(By.cssSelector("input#FirstName")).sendKeys(firstName);
-        driver.findElement(By.cssSelector("input#LastName")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
-        driver.findElement(By.cssSelector("input#Company")).sendKeys(company);
-        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
-        driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys(password);
+        driver.findElement(By.id("_r_2_"));
 
-        driver.findElement(By.cssSelector("button#register-button")).click();
-        Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
+
     }
 
     @Test
     public void TC_02_Register() {
-        driver.findElement(By.cssSelector("a.ico-logout")).click();
-        driver.findElement(By.cssSelector("a.ico-login")).click();
-        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
-        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
-        driver.findElement(By.cssSelector("button.login-button")).click();
+        System.out.println("hello");
 
-        Assert.assertTrue(driver.findElement(By.xpath("//a[@class='ico-account' and text()='My account']")).isDisplayed());
     }
 
     @Test
     public void TC_03_Register() {
+        System.out.println("world");
 
-        driver.findElement(By.cssSelector("a.ico-account")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("input#gender-female")).isSelected());
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#FirstName")).getAttribute("value"), firstName);
-
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#LastName")).getAttribute("value"), lastName);
-
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#Email")).getAttribute("value"), emailAddress);
-
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#Company")).getAttribute("value"), company);
 
     }
 
@@ -78,11 +49,7 @@ public class Level_01_Repeat_Yourself {
         driver.quit();
     }
 
-    private int generateRandomNumber() {
-        Random rand = new Random();
-        int randomNumber = rand.nextInt(99999);
-        return randomNumber;
-    }
+
 
 }
 
